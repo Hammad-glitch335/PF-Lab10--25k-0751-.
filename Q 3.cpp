02b180ea6d4c3d934b4b50a3f219387ef62669f1
Muhammad Hammad <k250751@nu.edu.pk>
2025-11-10 11:04:38 +0500

    Q 3.cpp

--- Q 3.cpp
+++ Q 3.cpp
@@ -1,9 +1,28 @@
 #include <stdio.h>
-int main(){
-	char ch;
-	FILE *ptr;
-	ptr = fopen("taskFile.txt", "r");
-	while ((ch = fgetc(ptr)) != EOF){
-		printf("%c", ch);
-	}
+#include <conio.h>
+
+int main()
+{
+    FILE *ptr;
+    int ch;
+    ptr = fopen("text.txt", "w");
+    fprintf(ptr, "File initialization success!\n");
+    fclose(ptr);
+    ptr = fopen("text.txt", "r");
+    while((ch = fgetc(ptr)) != EOF)
+    {
+        printf("%c", ch);
+    }
+    fclose(ptr);
+    ptr = fopen("text.txt", "a");
+    fprintf(ptr, "New data appended to file!\n");
+    fclose(ptr);
+    ptr = fopen("text.txt", "r");
+    while((ch = fgetc(ptr)) != EOF)
+    {
+        printf("%c", ch);
+    }
+    fclose(ptr);
+    getch();
+    return 0;
 }
